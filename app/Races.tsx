82d967e84dd4247702ccac703489a88f1ed3c85a
raggiadolf <ragnar.adolf@gmail.com>
2024-03-10 16:33:32 +0000

    jersey ordering

--- app/Races.tsx
+++ app/Races.tsx
@@ -3,8 +3,10 @@ import OldJersey from "./icons/OldJersey";
 import PolkaDotJersey from "./icons/PolkaDotJersey";
 import YellowJersey from "./icons/YellowJersey";
 import prisma from "./lib/db";
-import { parseISO, format } from "date-fns";
+import { format } from "date-fns";
 import Image from "next/image";
+
+const jerseyOrder = ["YELLOW", "OLD", "POLKA", "GREEN"];
 
 export default async function Races() {
   const races = await prisma.race.findMany({
@@ -41,39 +43,49 @@ export default async function Races() {
             <dl className="flex w-full flex-none justify-between sm:w-auto">
               <div className="isolate flex overflow-hidden">
                 <dt className="sr-only">Participants</dt>
-                {race.Participant.filter((p) => p.jersey).map((p) => (
-                  <dd key={p.id} className="relative inline-block">
-                    <Image
-                      className="relative inline-block rounded-full ring-2 ring-white"
-                      width={40}
-                      height={40}
-                      src={p.User.profile || ""}
-                      alt={p.User.firstname || ""}
-                    />
-                    {(() => {
-                      switch (p.jersey) {
-                        case "YELLOW":
-                          return (
-                            <YellowJersey className="h-4 w-4 absolute bottom-0 left-0 block" />
-                          );
-                        case "GREEN":
-                          return (
-                            <GreenJersey className="h-4 w-4 absolute bottom-0 left-0 block" />
-                          );
-                        case "POLKA":
-                          return (
-                            <PolkaDotJersey className="h-4 w-4 absolute bottom-0 left-0 block" />
-                          );
-                        case "OLD":
-                          return (
-                            <OldJersey className="h-4 w-4 absolute bottom-0 left-0 block" />
-                          );
-                        default:
-                          return null;
-                      }
-                    })()}
-                  </dd>
-                ))}
+                {race.Participant.filter((p) => p.jersey)
+                  .sort((a, b) => {
+                    return (
+                      jerseyOrder.indexOf(a.jersey || "") -
+                      jerseyOrder.indexOf(b.jersey || "")
+                    );
+                  })
+                  .map((p) => (
+                    <dd key={p.id} className="relative inline-block">
+                      <Image
+                        className="relative inline-block rounded-full ring-2 ring-white"
+                        width={40}
+                        height={40}
+                        src={p.User.profile || ""}
+                        alt={p.User.firstname || ""}
+                      />
+                      {(() => {
+                        switch (p.jersey) {
+                          case "YELLOW":
+                            return (
+                              <YellowJersey className="h-4 w-4 absolute bottom-0 left-0 block" />
+                            );
+                          case "GREEN":
+                            return (
+                              <GreenJersey className="h-4 w-4 absolute bottom-0 left-0 block" />
+                            );
+                          case "POLKA":
+                            return (
+                              <PolkaDotJersey className="h-4 w-4 absolute bottom-0 left-0 block" />
+                            );
+                          case "OLD":
+                            return (
+                              <OldJersey className="h-4 w-4 absolute bottom-0 left-0 block" />
+                            );
+                          default:
+                            return null;
+                        }
+                      })()}
+                    </dd>
+                  ))}
+              </div>
+              <div className="ml-2">
+                + {race.Participant.filter((p) => !p.jersey).length}
               </div>
             </dl>
           </li>
