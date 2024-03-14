8199076f063f1617458bae67bd1be9fd15f5b017
raggiadolf <ragnar.adolf@gmail.com>
2024-03-14 22:31:51 +0000

    some sort of navbar I guess

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,26 +1,61 @@
 "use client";
 import type { User } from "@prisma/client";
 import Image from "next/image";
-import { Tabs, Tab } from "@nextui-org/react";
+import {
+  Navbar,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Avatar,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  DropdownTrigger,
+} from "@nextui-org/react";
 import { usePathname } from "next/navigation";
+import classNames from "../utils";
 
-export function NavBar({ user }: { user: User }) {
+export default function NavBar({ user }: { user: User | null }) {
   const pathname = usePathname();
   return (
-    <Tabs
-      selectedKey={pathname}
-      color="primary"
-      variant="bordered"
-      aria-label="Navigation"
-    >
-      <Tab key="home" href="/" title={<CalendarIcon />} />
-      <Tab key="about" href="/about" title={<TrophyIcon />} />
-      <Tab key="profile" href="/profile" title={<ProfileIcon user={user} />} />
-    </Tabs>
+    <Navbar className="sticky bottom-0">
+      <NavbarContent justify="center" className="w-full space-x-16">
+        <NavbarItem>
+          <Link href="/races">
+            <CalendarIcon isActive={pathname === "/races"} />
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="/leaderboard">
+            <TrophyIcon isActive={pathname === "/leaderboard"} />
+          </Link>
+        </NavbarItem>
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              name={user?.firstname || ""}
+              size="sm"
+              src={user?.profile || ""}
+            />
+          </DropdownTrigger>
+          <DropdownMenu>
+            <DropdownItem>
+              <Link href="/profile">Profile</Link>
+            </DropdownItem>
+            <DropdownItem>
+              <Link href="/logout">Logout</Link>
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </NavbarContent>
+    </Navbar>
   );
 }
 
-function CalendarIcon({ props }: { props?: any }) {
+function CalendarIcon({ isActive }: { isActive: boolean }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -28,8 +63,10 @@ function CalendarIcon({ props }: { props?: any }) {
       viewBox="0 0 24 24"
       strokeWidth="1.5"
       stroke="currentColor"
-      className="w-8 h-8 text-gray-600"
-      {...props}
+      className={classNames(
+        "w-8 h-8",
+        isActive ? "text-black" : "text-gray-500"
+      )}
     >
       <path
         strokeLinecap="round"
@@ -40,7 +77,7 @@ function CalendarIcon({ props }: { props?: any }) {
   );
 }
 
-function TrophyIcon({ props }: { props?: any }) {
+function TrophyIcon({ isActive }: { isActive: boolean }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -48,8 +85,10 @@ function TrophyIcon({ props }: { props?: any }) {
       viewBox="0 0 24 24"
       strokeWidth="1.5"
       stroke="currentColor"
-      className="w-8 h-8 text-gray-600"
-      {...props}
+      className={classNames(
+        "w-8 h-8",
+        isActive ? "text-black" : "text-gray-500"
+      )}
     >
       <path
         strokeLinecap="round"
@@ -57,19 +96,5 @@ function TrophyIcon({ props }: { props?: any }) {
         d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0"
       />
     </svg>
-  );
-}
-
-function ProfileIcon({ user, props }: { user: User; props?: any }) {
-  if (!user) return null; // TODO: Return default profile icon
-  return (
-    <Image
-      className="inline-block rounded-full"
-      width={40}
-      height={40}
-      src={user.profile || ""}
-      alt=""
-      {...props}
-    />
   );
 }
