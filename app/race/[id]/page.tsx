3975979d03dc2f8bc28dcb434af0b30685ceb355
raggiadolf <ragnar.adolf@gmail.com>
2024-03-17 15:25:00 +0000

    console log for race

--- app/race/[id]/page.tsx
+++ app/race/[id]/page.tsx
@@ -1,18 +1,25 @@
 import RaceTable from "@/app/components/RaceTable";
 
 export default async function Race({ params }: { params: { id: string } }) {
-  const race = await prisma?.race.findUniqueOrThrow({
-    where: {
-      id: parseInt(params.id),
-    },
-    include: {
-      Participant: {
-        include: {
-          User: true,
+  let race;
+  try {
+    race = await prisma?.race.findUniqueOrThrow({
+      where: {
+        id: parseInt(params.id),
+      },
+      include: {
+        Participant: {
+          include: {
+            User: true,
+          },
         },
       },
-    },
-  });
+    });
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+  console.log("race", race);
   if (!race) return null; // TODO: Empty state
   return <RaceTable race={race} />;
 }
