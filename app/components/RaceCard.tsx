2d464156e19fa6757021aca911e05ef4b2c17130
raggiadolf <ragnar.adolf@gmail.com>
2024-03-18 16:58:03 +0000

    attendancepill styling

--- app/components/RaceCard.tsx
+++ app/components/RaceCard.tsx
@@ -21,7 +21,7 @@ export default async function RaceCard({ id }: { id: number }) {
           </Link>
         </div>
         {user && (
-          <div className="-mt-2">
+          <div className="-mt-3 -mr-3">
             <AttendancePill userId={user.id} raceId={race.id} />
           </div>
         )}
