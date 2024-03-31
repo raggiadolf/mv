4cadcb2614da85eb7711745bafd1dad2d8ab4420
Ragnar Árnason <ragnar.adolf@gmail.com>
2024-03-31 16:10:13 +0000

    welcome modal on first enter to log in with strava

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,5 @@
 import { redirect } from "next/navigation"
-import { validateRequest } from "./lib/auth"
 
 export default async function Page() {
-  // const { user } = await validateRequest()
-  // if (!user) {
-  //   return redirect("/login");
-  // } else {
   return redirect("/races")
-  // }
-  // return (
-  //   <main className="flex min-h-screen flex-col items-center justify-between bg-gray-100 p-2">
-  //     <div className="relative flex place-items-center">
-  //       <h1>Hi, {user.firstname}</h1>
-  //       <p>Your user ID is {user.id}.</p>
-  //       <form action={logout}>
-  //         <button>Sign out</button>
-  //       </form>
-  //     </div>
-  //   </main>
-  // );
 }
--- app/races/page.tsx
+++ app/races/page.tsx
@@ -1,17 +1,23 @@
+import WelcomeLoginModal from "../components/WelcomeLoginModal"
 import RaceCard from "../components/RaceCard"
+import { validateRequest } from "../lib/auth"
 import { getAllRaces } from "../queries/db"
 
 export default async function Races() {
   const races = await getAllRaces()
+  const { user } = await validateRequest()
   return (
-    <ul role="list" className="space-y-4 w-full max-w-[500px]">
-      {races.map((race) => {
-        return (
-          <li key={race.id}>
-            <RaceCard id={race.id} />
-          </li>
-        )
-      })}
-    </ul>
+    <>
+      <ul role="list" className="space-y-4 w-full max-w-[500px]">
+        {races.map((race) => {
+          return (
+            <li key={race.id}>
+              <RaceCard id={race.id} />
+            </li>
+          )
+        })}
+      </ul>
+      <WelcomeLoginModal user={user} />
+    </>
   )
 }
