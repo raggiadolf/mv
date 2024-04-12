55ffe39f1f09582f8958fe5385b3f9e735b0ea9a
Ragnar Árnason <ragnar.adolf@gmail.com>
2024-04-12 21:54:03 +0000

    remove cron

--- app/cron/route.ts
+++
@@ -1,9 +0,0 @@
-export default function GET(req: Request, res: Response) {
-  if (
-    req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
-  ) {
-    return new Response("Unauthorized", { status: 401 })
-  }
-  console.log("cron job")
-  return new Response("OK", { status: 200 })
-}
