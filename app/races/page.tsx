a6e6087170b459f5409866c58daa8782a6b98343
Ragnar Árnason <ragnar.adolf@gmail.com>
2024-04-21 18:51:21 +0000

    admins see empty races

--- app/races/page.tsx
+++ app/races/page.tsx
@@ -2,6 +2,7 @@ import WelcomeLoginModal from "../components/WelcomeLoginModal"
 import RaceCard from "../components/RaceCard"
 import { validateRequest } from "../lib/auth"
 import { getAllRaces } from "../queries/db"
+import { satisfiesRole } from "../lib/utils"
 
 export default async function Races({
   searchParams,
@@ -12,11 +13,12 @@ export default async function Races({
 }) {
   const races = await getAllRaces(searchParams.season)
   const { user } = await validateRequest()
+  const isAdmin = satisfiesRole("ADMIN", user)
   return (
     <>
       <ul role="list" className="space-y-4 w-full max-w-[500px]">
         {races
-          .filter((r) => r.Participant.length > 0)
+          .filter((r) => isAdmin || r.Participant.length > 0)
           .map((race) => {
             return (
               <li key={race.id}>
